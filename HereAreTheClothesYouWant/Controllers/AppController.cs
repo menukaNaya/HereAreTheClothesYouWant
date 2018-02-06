76ecb1d2f74654e1963b76de4c1bc02a033a0551
Menuka Weerasooriya <mnw@tiqri.com>
2018-02-06 10:06:41 +0530

    More works..

--- HereAreTheClothesYouWant/Controllers/AppController.cs
+++ HereAreTheClothesYouWant/Controllers/AppController.cs
@@ -15,5 +15,11 @@ namespace HereAreTheClothesYouWant.Controllers
         {
             return View();
         }
+
+        public IActionResult Products()
+        {
+            ViewBag.Title = "Our Products";
+            return View();
+        }
     }
 }
